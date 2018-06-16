84ef41845b16cc7b93f4cf4c5d2a99f1916ab194
nayname <nayname@gmail.com>
2018-06-17 00:13:42 +0200

    COmplete map of posts-answers

--- parse/src/main/java/tmt/snippets/Analyze.java
+++ parse/src/main/java/tmt/snippets/Analyze.java
@@ -19,11 +19,18 @@ import tmt.utils.Utils;
 
 public class Analyze {
   static HashMap<Integer, ArrayList<Row>> answers;
+  static HashMap<Row, ArrayList<Row>> complete;
 
   public static void main(String[] args) throws JsonIOException, JsonSyntaxException, FileNotFoundException {
     Gson gson = new Gson();
     
     File f = new File(Conf.answers_output.replace("?", "_all"));
     answers = gson.fromJson(new FileReader(f), Conf.gson_answers);
+        ArrayList<Row> posts = new ArrayList<Row>();
+        f = new File(Conf.posts_output.replace("?", "_all"));
+            posts = gson.fromJson(new FileReader(f), Row[].class);
+
+    for (Row p : posts)
+       complete.put(p., answers.get(p)) 
   }
 }
