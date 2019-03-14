b60df31b23d1bf12e037975ab9c682f6b381c9d9
Виктор Зайцев <kreker92@MacBook-Air-Viktor.local>
2019-03-15 00:25:25 +0300

    Get response from server when extension is launched

--- js_plugin/extension.js
+++ js_plugin/extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const editor = vscode.window.activeTextEditor;
+const rp = require("request-promise");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,29 +10,37 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
+
 function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "contexttocode" is now active!');
+	console.log('"showText" active');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		var mes = "Hello World!\n111"
-		vscode.window.showInformationMessage(mes);
+	const commandId = 'extension.showText'
+	let disposable = vscode.commands.registerCommand(commandId, function () {
+		vscode.window.showInformationMessage('Ждём ответ от сервера');
+		const text = editor.document.getText()
+		rp('http://78.46.103.68:8081/')
+			.then(function (htmlString) {
+				vscode.window.showInformationMessage(htmlString);
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
 	});
-
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
+
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+	console.log('showText disactive');
+}
 
 module.exports = {
 	activate,
