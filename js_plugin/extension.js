dbda832aa6f82a2f0b63154f087c2736bfe84487
Виктор Зайцев <kreker92@MacBook-Air-Viktor.local>
2019-03-19 18:28:33 +0300

    Покрасивше. Вставляет текст где курсор.

--- js_plugin/extension.js
+++ js_plugin/extension.js
@@ -34,7 +34,7 @@ function activate(context) {
 		}
 		rp(options)
 			.then(function (htmlString) {
-				var panel = vscode.window.createWebviewPanel(
+				const panel = vscode.window.createWebviewPanel(
 					'tips', // Identifies the type of the webview. Used internally
 					'Предлагаемые варианты', // Title of the panel displayed to the user
 					vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
@@ -43,17 +43,71 @@ function activate(context) {
 						enableScripts: true
 					  } // Webview options. More on these later.
 				  );
-				  panel.webview.html = htmlString;
-				  // Handle messages from the webview
+					const mes = `
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Подсказки</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<style>
+			p, button, div {
+				color: #DDD;
+			}
+			hr {
+				border-top: 1px solid rgba(255,255,255,.1);
+			}
+		</style>
+		<script>
+			const text_ = 'SOME CODE';
+			const vscode = acquireVsCodeApi();
+			function useAdvise(){
+				console.log(vscode);
+				vscode.postMessage({command: 'use',text: text_})
+			}
+			function hideAdvise(){
+				vscode.postMessage({command: 'hide'})
+			}
+		</script>
+	</head>
+	<body class="bg-dark">
+		<div class="container-fluid">
+			<hr />
+			<div class="code-wrapper">
+			<a href="http://somegreatsite.com">Link Name</a> is a link to another nifty site.
+			<h1>This is a Header</h1>
+			<h2>This is a Medium Header</h2>
+			<p>Send me mail at <a href="mailto:support@yourcompany.com">support@yourcompany.com</a></p>.
+			<p>This is a new paragraph!</p> <b>This is a new paragraph!</b><br /> <b><i>This is a new sentence without a paragraph break, in bold italics.</i></b>
+			</div>
+			<hr />
+			<button class="btn btn-outline-light btn-block" onclick="useAdvise()">Ok</button>
+			<button class="btn btn-dark btn-block" onclick="hideAdvise()">Скрыть</button>
+		</div>
+	</body>
+</html>
+					`;
+					panel.webview.html = mes; // htmlString;
+					// Handle messages from the webview
 				  panel.webview.onDidReceiveMessage(
 					message => {
 					  switch (message.command) {
-						case 'info':
+						case 'use':
+							console.log('use');
 							editor.edit(edit => {
-								edit.insert(new vscode.Position(0, 0), message.text);
+								let pos = new vscode.Position(editor.selection.start.line,
+																							 editor.selection.start.character)
+								edit.insert(pos, message.text);
 							});
 						  return;
-					  }
+						case 'hide':
+							console.log('hide');
+							return;
+						}
 					},
 					undefined,
 					context.subscriptions
