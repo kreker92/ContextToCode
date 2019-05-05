d86990124a028295a7c86f4c3f6fb6edb9728710
nayname <nayname@gmail.com>
2019-05-05 21:52:58 +0200

    Data preparation

--- data_generator/lang_scripts/js.py
+++ data_generator/lang_scripts/js.py
@@ -10,6 +10,77 @@ def is_valid (value):
     else:
         return True
 
+def get_inner_types(node, tmp):
+    if "CallExpression" != node["type"]:
+        if "value" in node:
+	        tmp.append({"type":node["type"], "value":node["value"]})
+        else:
+            tmp.append({"type":node["type"], "value":""})
+
+    if "links" in node:
+        for child in node["links"]:
+            get_inner_types(child, tmp)
+
+    return tmp
+		
+def get_type (node):
+    if node["type"] == "CallExpression":
+        tmp = []
+        return get_inner_types(node, tmp)
+    else:		
+        return node["type"]
+		
+#def in_memberexpression(node, ast_raw):
+#    id = ast_raw[node]["id"]
+	
+#    for key, value in ast_raw.items():
+#        if "children" in value and id in value["children"] and value["type"] == :
+#            return true;
+
+#    return false;
+		
+def get_parent_in_scope(node, value, ast_raw, vars, level):
+    parent = get_parent(node, ast_raw)
+    if value+":"+str(parent) in vars:
+        return value+":"+str(parent)
+
+    if level > 2 or parent == 0:
+        return "";
+
+    return get_parent_in_scope(parent, value, ast_raw, vars, level+1)
+		
+def get_parent(node, ast_raw):
+    id = ast_raw[node]["id"]
+    parent = 0
+	
+    for key, value in ast_raw.items():
+        if "children" in value and id in value["children"]:
+            parent = value["id"]
+			
+    if parent == 0 or not is_valid(ast_raw[parent]):
+        return parent
+    else:
+        return get_parent(parent, ast_raw)
+
+def fill_vars(vars, ast_raw):
+    for key, value in ast_raw.items():
+        if "children" in value and value["type"] == "VariableDeclaration":
+            for child in value["children"]:
+                 if ast_raw[child]["type"] == "VariableDeclarator":				 
+                     if ast_raw[child]["value"] in vars:
+                         print(ast_raw[child]["value"])
+                         #ddd()
+                     if "children" in ast_raw[child]:
+                         for inner_child in ast_raw[child]["children"]:
+                             if ast_raw[inner_child]["type"] != "Identifier":
+                                 vars[ast_raw[child]["value"]+":"+str(get_parent(child, ast_raw))] = get_type(ast_raw[inner_child])
+                 elif "children" in ast_raw[child]:
+                     in_block += ast_raw[child]["children"]
+   # if value["type"] == "Identifier" and not value["value"] in vars:
+    #    for key, value in ast_raw.items():
+     #       if value["type"] == "VariableDeclaration":
+      #  vars[value["value"]] =
+		
 def is_end_node (value, ids):
     if value["type"] == "LiteralString":
         value["value"] = "-"
@@ -39,7 +110,8 @@ def get_ids(ids, value):
 
 def parse_file (src, dst, o_dst, data):
 #and value["type"] == "VariableDeclaration"
-    #print(src)
+    print(src)
+    vars = {}
     ast_raw = {}
     ast_out = []
     ids = []
@@ -57,8 +129,16 @@ def parse_file (src, dst, o_dst, data):
                      value["links"].append(ast_raw[child])
                  elif "children" in ast_raw[child]:
                      in_block += ast_raw[child]["children"]
-#    print(in_block)
+    fill_vars(vars, ast_raw)
     for key, value in ast_raw.items():
+        if value["type"] == "Identifier":
+         #   if in_memberexpression(key, ast_raw):
+         #       vars[value["value"]+":"+str(get_parent(key, ast_raw))] = "MemberExpression"
+            parent = get_parent_in_scope(key, value["value"], ast_raw, vars, 0)
+            if parent:
+                vars[value["value"]+":"+str(get_parent(key, ast_raw))] = vars[parent]
+            else:
+                vars[value["value"]+":"+str(get_parent(key, ast_raw))] = ""
         if is_end_node(value, ids):
            # get_text(value)
             if "links" in value:
@@ -69,10 +149,11 @@ def parse_file (src, dst, o_dst, data):
 
    # with open('/root/ContextToCode/data_generator/lang_scripts/data_raw.json', 'w') as outfile:
    #     json.dump(ast_raw, outfile)
+    print(vars)
     with open(dst.strip(), 'w') as outfile:
         json.dump(ast_out, outfile)
     with open(o_dst.strip(), 'w') as outfile:
-        json.dump(parsed, outfile)
+        outfile.write(data)
 
 def main(arg):	
     if arg[0] == "file":
@@ -84,16 +165,17 @@ def main(arg):
         #f = open("/root/js/data_picks/picked", "r")
         #check = f.read()
 			
-        with open("/root/js/programs_eval1.json", 'r') as f: 
+        with open("/root/js/programs_eval.json", 'r') as f: 
             jsons = f.readlines()
 
-        with open("/root/js/programs_eval1.txt", 'r') as f: 
+        with open("/root/js/programs_eval.txt", 'r') as f: 
             files = f.readlines()
 
         count = 0
-        print(len(jsons))
-        print(len(files))
+        #print(len(jsons))
+        #print(len(files))
         for i in range(len(files)):
+            i+=1
             name = files[i].split('/')[-1]
             k = files[i].rfind("data/")
             file = files[i][:k] + "/root/js/data/" + files[i][k+5:]
@@ -101,11 +183,12 @@ def main(arg):
                 #data = json.loads(jsons[i])
                 f = open("/root/js/data_picks/mask", "a")
                 f.write(str(count)+", "+file)
-                try:
+                #try:
         #            if (count < 5):
-                    parse_file(file, "/root/js/data_picks/sandbox/"+str(count), "/root/js/data_picks/originals/"+str(count), jsons[i])#"/root/js/data_picks/sandbox/"+name.strip().replace(".js", #"-raw.js"))
-                except:
-                    print("Can't parse file"+file)
+                parse_file(file, "/root/js/data_picks/sandbox_test/"+str(count), "/root/js/data_picks/originals_test/"+str(count), jsons[i])#"/root/js/data_picks/sandbox/"+name.strip().replace(".js", #"-raw.js"))
+               # except:
+                #    print("Can't parse file"+file)
+                ddd()
                 count += 1
 					
 if __name__ == "__main__":
