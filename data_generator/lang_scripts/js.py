7b8dfc143b3ba3394886c434b7afae84c9b5aaad
Vladimir Tarasov <vladimir.tarasov@gmail.com>
2019-06-07 11:58:01 +0300

    type detection start

--- data_generator/lang_scripts/js.py
+++ data_generator/lang_scripts/js.py
@@ -4,18 +4,15 @@ from shutil import copyfile
 import subprocess
 import sys
 
+def _print(obj):
+    print(json.dumps(obj,indent=2))
+
 def is_valid (value):
-    if value["type"] == "BlockStatement":
-        return False
-    else:
-        return True
-		
+    return value["type"] != "BlockStatement";
+
 def is_with_links (value):
-    if value["type"] == "FunctionExpression":
-        return False
-    else:
-        return True
-		
+    return value["type"] != "FunctionExpression";
+
 def contains_key (key, node):
     if node["type"] == key["type"] and (not "value" in key or node["value"] == key["value"]) and (not "source" in key or ("source" in node and node["source"] == key["source"])):
         return True;
@@ -23,9 +20,9 @@ def contains_key (key, node):
         for child in node["links"]:
             if (contains_key(key, child)):
                 return True
-				
+
     return False
-	
+
 def get_complex_types(node):
     with open("/root/ContextToCode/data/datasets/js/templates", 'r') as handle:
         anchors = json.load(handle)
@@ -38,25 +35,29 @@ def get_complex_types(node):
                     break;
         if contains_anchor:
             return anchor["ast_type"]
-			
+
     return ""
-		
+
 def get_type (node):
+
+    print (node['type']);
+    # print (node.value);
+    return;
     if node["type"] == "CallExpression":
         print(str(get_complex_types(node)))
         return str(get_complex_types(node))
-    else:		
+    else:
         return node["type"]
-		
+
 #def in_memberexpression(node, ast_raw):
 #    id = ast_raw[node]["id"]
-	
+
 #    for key, value in ast_raw.items():
 #        if "children" in value and id in value["children"] and value["type"] == :
 #            return true;
 
 #    return false;
-		
+
 def get_parent_in_scope(parent, value, ast_raw, vars, level):
     if value+":"+str(parent) in vars:
         return value+":"+str(parent)
@@ -65,19 +66,19 @@ def get_parent_in_scope(parent, value, ast_raw, vars, level):
         return "";
 
     return get_parent_in_scope(ast_raw[parent]["parent"], value, ast_raw, vars, level+1)
-		
+
 def set_parent(ast_raw, id, parent):
     ast_raw[id]["parent"] = parent
     if not is_valid(ast_raw[id]):
-        parent = id  
+        parent = id
     if "children" in ast_raw[id]:
         for child in ast_raw[id]["children"]:
             set_parent(ast_raw, ast_raw[child]["id"], parent)
-			
+
 def get_inner_types(node, tmp):
     if "CallExpression" != node["type"]:
         if "value" in node:
-	        tmp.append({"type":node["type"], "value":node["value"]})
+            tmp.append({"type":node["type"], "value":node["value"]})
         else:
             tmp.append({"type":node["type"], "value":""})
 
@@ -86,7 +87,51 @@ def get_inner_types(node, tmp):
             get_inner_types(child, tmp)
 
     return tmp
-			
+
+from ref import js_fuctions
+
+def find_type(expr):
+
+    if (len(expr['links'])>0):
+
+        child = expr['links'][0]
+
+        if ('value' in child) and (child['value'] in js_fuctions.keys()):
+            return js_fuctions[child['value']];
+
+        if (len(expr['links'])==1):
+            if (child['links'][-1] == 'toString'):
+                return 'String';
+
+    return 'Unknown';
+
+# Типы в ast и типы данных в JS
+types_dict = {
+    'LiteralString'     : 'String',
+    'LiteralNumber'     : 'Number',
+    'LiteralBoolean'    : 'Boolean',
+    'LiteralNull'       : 'Null',
+    'ObjectExpression'  : 'Object',
+    'ArrayExpression'   : 'Array'
+}
+
+def set_types(ast_raw):
+
+    for k, v in ast_raw.items():
+        if (v['type'] == 'VariableDeclarator') and ('children' in v):
+            if (len(v['links'])>0):
+                child = v['links'][0];
+                if (child['type'] in types_dict.keys()):
+                    v['vartype'] = types_dict[child['type']]
+                elif (child['type'] == 'CallExpression'):
+                    v['vartype'] = find_type(child)
+            else: # v['links'] is empty (). This means only
+                v['vartype'] = 'Function'
+            # break;
+
+
+    return []
+
 def set_vars(vars, ast_raw):
     assigned = False;
     for _, value in ast_raw.items():
@@ -94,8 +139,8 @@ def set_vars(vars, ast_raw):
             for child in value["children"]:
                  key = ast_raw[child]["value"]+":"+str(ast_raw[child]["parent"])
                  if not key in vars or not vars[key]["type"]:
-                     if ast_raw[child]["type"] == "VariableDeclarator":	
-                    # if not key in vars:				 
+                     if ast_raw[child]["type"] == "VariableDeclarator":
+                    # if not key in vars:
                          if ast_raw[child]["value"] in vars:
                              print(ast_raw[child]["value"])
                              #ddd()
@@ -105,18 +150,20 @@ def set_vars(vars, ast_raw):
                                      vars[key] = {"type":get_type(ast_raw[inner_child]), "links":get_inner_types(ast_raw[inner_child], [])}
                                      if get_type(ast_raw[inner_child]):
                                          assigned = True;
-    count = 0;										 
+
+    count = 0;
     for key, value in ast_raw.items():
         if value["type"] == "Identifier" or value["type"] == "VariableDeclarator":
             parent = get_parent_in_scope(value["parent"], value["value"], ast_raw, vars, 0)
             if parent in vars:
                 value["source"] = vars[parent]["type"]
-                if (vars[parent]["type"])
-                    count += 1
                 value["source_info"] = vars[parent]["links"]
+                if (vars[parent]["type"]):
+                    count += 1
+
     print(count)
     return assigned
-		
+
 def is_end_node (value, ids):
     if value["type"] == "LiteralString":
         value["value"] = "-"
@@ -133,7 +180,7 @@ def get_text(node):
     else:
         if "links" in node:
             for child in node["links"]:
-                node["value"] += " "+get_text(child) 
+                node["value"] += " "+get_text(child)
         return node["value"]
 
 def get_ids(ids, value):
@@ -143,40 +190,55 @@ def get_ids(ids, value):
            get_ids(ids, child)
        else:
            ids.append(child["id"])
-		   
+
 def set_links(ast_raw, in_block):
     for key, value in ast_raw.items():
         if "children" in value:
             value["links"] = []
             for child in value["children"]:
-                 if is_valid(ast_raw[child]) and is_with_links(ast_raw[child]):				 
+
+                 if is_valid(ast_raw[child]) and is_with_links(ast_raw[child]):
                      value["links"].append(ast_raw[child])
                  elif "children" in ast_raw[child]:
                      in_block += ast_raw[child]["children"]
 
 def parse_file (src, dst, o_dst, data):
-#and value["type"] == "VariableDeclaration"
+
     ast_raw = {}
-    vars = {}
     ast_out = []
+
     ids = []
     in_block = []
     #with open(data, 'r') as handle:
     parsed = json.loads(data)
+
     for node in parsed:
         if node != 0:
             ast_raw[node['id']] = node
+
     set_links(ast_raw, in_block)
     set_parent(ast_raw, 0, 0)
-    while set_vars(vars, ast_raw):
-        continue
+
+    code_vars = get_vars(ast_raw)
+
+    # for _,x in code_vars.items():
+    #     print(x['value'])
+
+    set_types(ast_raw)
+
+    # while set_vars(vars, ast_raw):
+        # continue
+
+
+    return;
+
     for key, value in ast_raw.items():
         if not "parent" in value:
             ddd()
         if is_end_node(value, ids):
            # get_text(value)
             if "links" in value:
-       	        get_ids(ids, value)	
+                get_ids(ids, value)
             if value["id"] in in_block and value["type"] == "FunctionDeclaration":
                 value["type"] = "FunctionDeclarationInBlock"
             ast_out.append(value)
@@ -187,20 +249,33 @@ def parse_file (src, dst, o_dst, data):
     with open(o_dst.strip(), 'w') as outfile:
         outfile.write(data)
 
-def main(arg):	
+_DIR = os.path.dirname(__file__)
+
+def _path(relative):
+    return os.path.join(_DIR, relative)
+
+_NODE = 'nodejs' # or 'node'
+
+def main(arg):
     if arg[0] == "file":
-        parsed = subprocess.check_output(["/root/ContextToCode/data_generator/lang_scripts/js_parser/bin/js_parser.js", "/root/ContextToCode/data_generator/lang_scripts/input/input.js"])
-        parse_file("", "/root/ContextToCode/data_generator/lang_scripts/parsed", "/root/ContextToCode/data_generator/lang_scripts/o_parsed", parsed)#"/root/js/data_picks/sandbox/"+name.strip().replace(".js", "-raw.js"))
+
+        PARSER = _path('js_parser/bin/js_parser.js')
+        INPUT  = _path('input/input.js')
+
+        parsed = subprocess.check_output([_NODE, PARSER, INPUT])
+
+        parse_file("", _path('parsed'), _path('o_parsed'), parsed)
+
     elif arg[0] == "folder":
         #parse_file(file, "/root/js/data_picks/sandbox/"+str(count), jsons[i])#"/root/js/data_picks/sandbox/"+name.strip().replace(".js", "-raw.js"))
-		
+
         #f = open("/root/js/data_picks/picked", "r")
         #check = f.read()
-			
-        with open("/root/js/programs_eval.json", 'r') as f: 
+
+        with open("/root/js/programs_eval.json", 'r') as f:
             jsons = f.readlines()
 
-        with open("/root/js/programs_eval.txt", 'r') as f: 
+        with open("/root/js/programs_eval.txt", 'r') as f:
             files = f.readlines()
 
         count = 0
@@ -212,7 +287,7 @@ def main(arg):
             k = files[i].rfind("data/")
             file = files[i][:k] + "/root/js/data/" + files[i][k+5:]
             if True:#"/root/js/data/jsdelivr/jsdelivr/files/reactive.js/0.3.9/Ractive-legacy.runtime.js" in file:#files[i] in check:#os.path.isfile("/root/js/data_picks/1/"+name.strip()):
-                if (count > 0):
+                if (count > 2):
                     ddd()
                 #data = json.loads(jsons[i])
                 f = open("/root/js/data_picks/mask", "a")
@@ -222,7 +297,7 @@ def main(arg):
                # except:
                 #    print("Can't parse file"+file)
                 count += 1
-					
+
 if __name__ == "__main__":
    main(sys.argv[1:])
 
---
+++ data_generator/lang_scripts/ref.py
@@ -0,0 +1,13 @@
+
+# что возвращают функции
+js_fuctions = { #global
+
+  'isFinite'    : 'Boolean',
+  'isNaN'       : 'Boolean',
+  'parseFloat'  : 'Number',
+  'parseInt'    : 'Number',
+  'decodeURI'   : 'String',
+  'encodeURI'   : 'String',
+  'decodeURIComponent'  : 'String',
+  'encodeURIComponent'  : 'String'
+}
